409b05e60e59578282be94e20d2e6218eb160f2a
Kaseamsan Keawjeen <140032145+4Kasaje4@users.noreply.github.com>
2024-10-16 01:17:17 +0700

    Update DB_ICTCourseflow.sql

--- DB_ICTCourseflow.sql
+++ DB_ICTCourseflow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2024 at 08:27 AM
+-- Generation Time: Oct 15, 2024 at 08:16 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -62,7 +62,8 @@ INSERT INTO `course` (`course_ID`, `name_TH`, `name_EN`, `year`, `credit`, `sems
 ('227332', 'พื้นฐานปัญญาประดิษฐ์', 'Fundamental of Artificial Intelligence', '2565', '3(2-2-5)', 2, '3', '58c72864-de72-47af-ac6a-9201746717c7', 'f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef'),
 ('227361', 'การทดสอบซอฟต์แวร์', 'Software Testing', '2565', '3(2-2-5)', 1, '3', '58c72864-de72-47af-ac6a-9201746717c7', 'f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef'),
 ('227381', 'การจัดการโครงการซอฟต์แวร์', 'Software Project Management', '2565', '3(2-2-5)', 2, '3', '58c72864-de72-47af-ac6a-9201746717c7', 'f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef'),
-('227422', 'โครงงานทางวิศวกรรมซอฟต์แวร์', 'Software Engineering Project', '2565', '3(0-6-3)', 1, '4', '58c72864-de72-47af-ac6a-9201746717c7', 'f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef');
+('227422', 'โครงงานทางวิศวกรรมซอฟต์แวร์', 'Software Engineering Project', '2565', '3(0-6-3)', 1, '4', '58c72864-de72-47af-ac6a-9201746717c7', 'f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef'),
+('2d8cbe80-7eb3-4601-92ca-2ab6e135315a', 'ทดสอบรายวิชา', 'Sample Course', '2468', '3', 2, '1', '8475288f-f3b4-4813-8809-2c15fb4fbe22', 'ca3c1f7f-f197-4940-ba0c-9f31bffbeaf1');
 
 -- --------------------------------------------------------
 
@@ -82,7 +83,8 @@ CREATE TABLE `data_curriculum` (
 
 INSERT INTO `data_curriculum` (`data_curriculum_ID`, `curriculum_year`, `isActive`) VALUES
 ('0837f49c-f3ec-493b-bf32-a8e578b46285', '2560', 1),
-('58c72864-de72-47af-ac6a-9201746717c7', '2565', 0);
+('58c72864-de72-47af-ac6a-9201746717c7', '2565', 0),
+('8475288f-f3b4-4813-8809-2c15fb4fbe22', '2468', 1);
 
 -- --------------------------------------------------------
 
@@ -93,17 +95,17 @@ INSERT INTO `data_curriculum` (`data_curriculum_ID`, `curriculum_year`, `isActiv
 CREATE TABLE `major` (
   `major_ID` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `curriculum_name` varchar(255) NOT NULL,
-  `data_curriculum_ID` varchar(255) NOT NULL
+  `curriculum_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `major`
 --
 
-INSERT INTO `major` (`major_ID`, `name`, `curriculum_name`, `data_curriculum_ID`) VALUES
-('81b5235b-1710-4665-accb-89b87aee4213', 'คอมพิวเตอร์ธุรกิจ', 'หลักสูตรบริหารธุรกิจบัณฑิต สาขาวิชาคอมพิวเตอร์ธุรกิจ', '58c72864-de72-47af-ac6a-9201746717c7'),
-('f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef', 'วิศวกรรมซอฟต์แวร์', 'หลักสูตรวิศวกรรมศาสตรบัณฑิต สาขาวิชาวิศวกรรมซอฟต์แวร์', '58c72864-de72-47af-ac6a-9201746717c7');
+INSERT INTO `major` (`major_ID`, `name`, `curriculum_name`) VALUES
+('81b5235b-1710-4665-accb-89b87aee4213', 'คอมพิวเตอร์ธุรกิจ', 'หลักสูตรบริหารธุรกิจบัณฑิต สาขาวิชาคอมพิวเตอร์ธุรกิจ'),
+('ca3c1f7f-f197-4940-ba0c-9f31bffbeaf1', 'Test Major', 'Test curriculum name'),
+('f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef', 'วิศวกรรมซอฟต์แวร์', 'หลักสูตรวิศวกรรมศาสตรบัณฑิต สาขาวิชาวิศวกรรมซอฟต์แวร์');
 
 -- --------------------------------------------------------
 
@@ -150,7 +152,7 @@ CREATE TABLE `off_hour_doc` (
 INSERT INTO `off_hour_doc` (`off_hour_doc_ID`, `status`, `reasonCourseLeader`, `objective`, `numOfStudent`, `forStudentMajor`, `teacher`, `studyTime`, `oldRoom`, `newRoom`, `oldTime`, `newTime`, `oldSeat`, `newSeat`, `isCancelMidtermExam`, `isCancelFinalExam`, `cancelMidtermDate`, `cancelMidtermTime`, `cancelFinalDate`, `cancelFinalTime`, `reasonCancelExam`, `numOfSection`, `year`, `professor_ID`, `choiceTeacher`, `dateTimeSee`, `opencourse_ID`, `course_ID`, `major_ID`) VALUES
 ('05d5d79b-e0d4-4529-b914-6dd747770554', '1', '', '4', NULL, NULL, NULL, NULL, NULL, NULL, '', '', '', '', 1, 1, '2024-10-15', '12:00', '2024-10-18', '09:00', 'อาจารย์ติดภารกิจ', '', '', '757658f2-b954-4699-be4d-2ed4a7f28a54', 0, '1728973551180', 'fc28c548-89b3-4165-934a-1fef90187acd', NULL, NULL),
 ('15a60011-1699-4ed3-960c-fc5877a974f0', '2', 'ห้อง ICT1102 ไม่ว่าง', '2', NULL, NULL, '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', NULL, 'ICT1103/1', 'ICT1102', '08.00 - 12.00', '13.00 - 17.00', '150', '200', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', '2939b9ca-abdf-4221-a613-6f0e969ce215', 2, '1728973576076', 'b86323bf-44b2-4090-bf6b-839096d85a9e', NULL, NULL),
-('3f95a1e4-a993-4c9c-abc4-d44c6d75a63c', '0', NULL, '5', NULL, NULL, NULL, NULL, NULL, NULL, '', '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', '2939b9ca-abdf-4221-a613-6f0e969ce215', 0, NULL, NULL, '227101', NULL),
+('3f95a1e4-a993-4c9c-abc4-d44c6d75a63c', '1', '', '5', NULL, NULL, NULL, NULL, NULL, NULL, '', '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', '2939b9ca-abdf-4221-a613-6f0e969ce215', 0, '1728974560390', NULL, '227101', NULL),
 ('7bc374db-a875-42b6-b811-76a7bcb27fb4', '0', NULL, '1', 500, NULL, '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '08.00 - 12.00', NULL, NULL, '', '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '5', '2590', '2939b9ca-abdf-4221-a613-6f0e969ce215', 0, NULL, NULL, '227221', 'f0bfbb3f-9fc6-4983-b3f9-f81bf30863ef'),
 ('b16a8efe-5b0b-43a1-bcf9-81e4983a87bf', '0', NULL, '3', NULL, NULL, NULL, NULL, NULL, NULL, '', '', '500', '1000', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '', '', '2939b9ca-abdf-4221-a613-6f0e969ce215', 0, NULL, '2d95aa0a-0e77-40fd-a963-8857a7db4db4', NULL, NULL),
 ('d00374b7-9650-48e5-8871-4062fc746e8d', '0', NULL, '4', NULL, NULL, NULL, NULL, NULL, NULL, '', '', '', '', 1, 1, '2024-10-30', '10:00', '2024-12-25', '17:00', '', '', '', '2939b9ca-abdf-4221-a613-6f0e969ce215', 0, NULL, '86270407-a923-4fd6-a14e-75a98c1fadfe', NULL, NULL);
@@ -180,14 +182,16 @@ CREATE TABLE `opencourse` (
 --
 
 INSERT INTO `opencourse` (`openCourse_ID`, `numOfSection`, `numOfStudent`, `remark`, `isMidtermExam`, `isFinalExam`, `semster`, `education_year`, `teacher`, `professor_ID`, `course_ID`) VALUES
+('1e169ad7-637f-4aea-9fc8-ec0d6956424a', 2, 200, '', 1, 1, 2, '2590', '2939b9ca-abdf-4221-a613-6f0e969ce215', '757658f2-b954-4699-be4d-2ed4a7f28a54', '227101'),
 ('2158f867-56e7-43a4-8c72-aa8cf7eb570e', 2, 40, '-', 0, 1, 1, '2567', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '227151'),
 ('2d95aa0a-0e77-40fd-a963-8857a7db4db4', 5, 500, '', 0, 0, 1, '2590', '757658f2-b954-4699-be4d-2ed4a7f28a54', '2939b9ca-abdf-4221-a613-6f0e969ce215', '227422'),
 ('7e456864-76a6-44dd-a913-c3d988ca47d2', 3, 150, '-', 1, 1, 1, '2568', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '227101'),
 ('86270407-a923-4fd6-a14e-75a98c1fadfe', 2, 100, '', 1, 0, 1, '2590', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '2939b9ca-abdf-4221-a613-6f0e969ce215', '227141'),
+('a2145cd3-006b-444e-8e3d-f7083d84b71f', 2, 100, '', 1, 1, 2, '2590', '2939b9ca-abdf-4221-a613-6f0e969ce215', '2939b9ca-abdf-4221-a613-6f0e969ce215', '227101'),
 ('b86323bf-44b2-4090-bf6b-839096d85a9e', 3, 150, '', 1, 1, 1, '2590', '2939b9ca-abdf-4221-a613-6f0e969ce215', '2939b9ca-abdf-4221-a613-6f0e969ce215', '227101'),
 ('dae82c45-cca0-4fb4-9b9f-6ae04195a79a', 10, 200, '-', 1, 1, 2, '2570', '757658f2-b954-4699-be4d-2ed4a7f28a54', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '227141'),
 ('e20b2fd4-87ab-4dbe-aaa2-3d226f66e234', 5, 200, '-', 0, 0, 1, '2568', '2939b9ca-abdf-4221-a613-6f0e969ce215', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '227102'),
-('fc28c548-89b3-4165-934a-1fef90187acd', 11, 111, '1', 1, 0, 1, '11', '757658f2-b954-4699-be4d-2ed4a7f28a54', '757658f2-b954-4699-be4d-2ed4a7f28a54', '227101');
+('fc28c548-89b3-4165-934a-1fef90187acd', 2, 100, 'ไม่มี', 0, 0, 2, '2590', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '18561cf9-5904-41c8-ab4a-0de2e5ddc3db', '227101');
 
 -- --------------------------------------------------------
 
@@ -265,8 +269,7 @@ ALTER TABLE `data_curriculum`
 -- Indexes for table `major`
 --
 ALTER TABLE `major`
-  ADD PRIMARY KEY (`major_ID`),
-  ADD KEY `data_curriculum_ID` (`data_curriculum_ID`);
+  ADD PRIMARY KEY (`major_ID`);
 
 --
 -- Indexes for table `off_hour_doc`
@@ -308,12 +311,6 @@ ALTER TABLE `staff`
 ALTER TABLE `course`
   ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`data_curriculum_ID`) REFERENCES `data_curriculum` (`data_curriculum_ID`),
   ADD CONSTRAINT `course_ibfk_2` FOREIGN KEY (`major_ID`) REFERENCES `major` (`major_ID`);
-
---
--- Constraints for table `major`
---
-ALTER TABLE `major`
-  ADD CONSTRAINT `major_ibfk_1` FOREIGN KEY (`data_curriculum_ID`) REFERENCES `data_curriculum` (`data_curriculum_ID`);
 
 --
 -- Constraints for table `off_hour_doc`
